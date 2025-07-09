26760183aed95c7d09f0ab151fdb43da9fb4f2a3
winiiee <binita.hamal7@gmail.com>
2025-07-09 22:14:00 +0545

    added-full-stack-hq

--- workshops/04-scope/01-fullstack-hq/fullstack-hq.js
+++ workshops/04-scope/01-fullstack-hq/fullstack-hq.js
@@ -1,19 +1,21 @@
 // Edit the code below
+
 let place = 'Planet Earth';
 
 function fullstackHQ(){
   return middleFunction('United States');
 
-  function middleFunction(place) {
+  function middleFunction(place2) {
     return innerFunction('New York State');
 
-    function innerFunction(place) {
-      place = 'New York City';
+    function innerFunction(place3) {
+      let place4 = 'New York City';
       return innermostFunction();
 
       function innermostFunction() {
-        return 'Fullstack HQ is at ' + place + ', ' + place + ', ' + place + ', ' + place;
+        return 'Fullstack HQ is at ' + place + ', ' + place2 + ', ' + place3 + ', ' + place4;
       }
     }
   }
+  
 }

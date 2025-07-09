f338473cf528fc567cd578da52dfc50c67611283
winiiee <binita.hamal7@gmail.com>
2025-07-09 22:18:38 +0545

    added-exponentiate

--- workshops/02-loops/04-exponentiate/exponentiate.js
+++ workshops/02-loops/04-exponentiate/exponentiate.js
@@ -1,1 +1,14 @@
 // YOUR CODE BELOW
+//2**4 = 2*2*2*2
+function exponentiate(base,number){
+    let result = 1;
+    for(let i=1; i<=number; i++){
+        result = result*base;
+    }
+    return result;
+}
+
+let r = exponentiate(2,2)
+let r1 = exponentiate(3, 4)    // => 81
+console.log(r)
+console.log(r1)

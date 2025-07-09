57b186c0da144393291cb62b208c4b49428873e6
winiiee <binita.hamal7@gmail.com>
2025-07-09 22:18:48 +0545

    added-my-slice

--- workshops/02-loops/05-my-slice/my-slice.js
+++ workshops/02-loops/05-my-slice/my-slice.js
@@ -1,1 +1,30 @@
 // YOUR CODE BELOW
+
+function mySlice(originalString,startIdx,endIdx){
+    let newString = ""
+    if(endIdx === undefined && startIdx !== undefined){
+        for(let i=startIdx; i<originalString.length; i++){
+            newString = newString + originalString[i]
+        }
+    }
+    else if(startIdx !==undefined && endIdx !== undefined){
+        for(let i=startIdx; i<endIdx; i++){
+            newString = newString + originalString[i]
+        }
+
+    }
+    else{
+        newString = originalString;
+    }
+
+    return newString;
+    
+}
+
+let a =mySlice('slice and dice', 2)    // => ice and dice
+console.log(a)
+let b = mySlice('slice and dice', 2, 5)    // => ice
+console.log(b)
+
+let c = mySlice('slice and dice')    // => slice and dice
+console.log(c);

1092ef140c585a70f6e2370bba8a7838c7a75477
winiiee <binita.hamal7@gmail.com>
2025-07-09 22:16:20 +0545

    added-is-truthy

--- workshops/03-coercion-and-truthiness/02-is-truthy/is-truthy.js
+++ workshops/03-coercion-and-truthiness/02-is-truthy/is-truthy.js
@@ -1,1 +1,15 @@
 // YOUR CODE BELOW
+function isTruthy(a){
+    if(a){
+        return true;
+    }
+    else{
+        return `The ${a} value is falsey`;
+    }
+}
+
+
+isTruthy('I yearn for truth'); // => true
+isTruthy(null); // => The null value is falsey
+isTruthy(undefined)
+isTruthy(-1)

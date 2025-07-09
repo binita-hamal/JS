1b3dcf0b2f3ef9c21f237dda784583c0037d8ada
winiiee <binita.hamal7@gmail.com>
2025-07-09 22:17:56 +0545

    added-only-odds

--- workshops/02-loops/01-only-odds/only-odds.js
+++ workshops/02-loops/01-only-odds/only-odds.js
@@ -1,1 +1,23 @@
 // YOUR CODE BELOW
+
+function onlyOdds(num){
+    let sum =0;
+    if(num >0){
+        
+        for(let i=1; i<=num; i=i+2){
+            sum = sum + i;
+        }
+    }
+    else{
+        return 0;
+    }
+
+    return sum;
+}
+
+let re=onlyOdds(6); // => 9 (5 + 3 + 1)
+let re1 = onlyOdds(3)
+console.log(re)
+console.log(re1)
+let neg = onlyOdds(-1)
+console.log(neg)

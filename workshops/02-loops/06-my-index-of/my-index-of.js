acbd90c89e1c6ab9e0099b87667fcb53788413e2
winiiee <binita.hamal7@gmail.com>
2025-07-09 22:18:58 +0545

    added-my-index-of

--- workshops/02-loops/06-my-index-of/my-index-of.js
+++ workshops/02-loops/06-my-index-of/my-index-of.js
@@ -1,1 +1,11 @@
 // YOUR CODE BELOW
+
+function myIndexOf(string,searchValue,startIdx){
+    if(searchValue !== undefined){
+        console.log(searchValue[i])
+    }
+    
+}
+
+let a = myIndexOf('hello', 'h'); // => 0
+console.log(a);

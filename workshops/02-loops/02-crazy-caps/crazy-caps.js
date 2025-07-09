117798880fd1b4b72e14e82c157b97a7be5e3fe3
winiiee <binita.hamal7@gmail.com>
2025-07-09 22:18:08 +0545

    added-crazy-caps

--- workshops/02-loops/02-crazy-caps/crazy-caps.js
+++ workshops/02-loops/02-crazy-caps/crazy-caps.js
@@ -1,1 +1,27 @@
 // YOUR CODE BELOW
+// function crazyCaps(string){
+//     let newString = ""
+//     let firstLetter  = string[0].toLowerCase();
+//     for(let i =1; i<string.length; i++){
+//         newString  = (newString + string[i]).toUpperCase();
+//     }
+//     return firstLetter + newString;
+
+// }
+
+function crazyCaps(string){
+    let result ='';
+    for(let i=0; i<string.length; i++){
+        if(i%2 ===0){
+            result += string[i].toUpperCase();
+        }
+        else{
+            result += string[i].toLowerCase();
+        }
+        
+    }
+    return result;
+}
+
+let s = crazyCaps('fullstack is amazing'); // => fUlLsTaCk iS AmAzInG!
+console.log(s)

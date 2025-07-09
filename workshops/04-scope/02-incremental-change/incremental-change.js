6f4c35ecb7b9593b7f35cd1d7f82b4a8c6e65ff2
winiiee <binita.hamal7@gmail.com>
2025-07-09 22:14:20 +0545

    added-incremental-change

--- workshops/04-scope/02-incremental-change/incremental-change.js
+++ workshops/04-scope/02-incremental-change/incremental-change.js
@@ -1,3 +1,17 @@
 let counter = 0;
 
-// YOUR CODE BELOW
+function incrementalChange(num){
+    counter = counter + num;
+    return counter;
+}
+
+let c1 = incrementalChange(2);
+console.log(c1);
+
+let a=incrementalChange(2); // => 2
+console.log(a);
+let c =incrementalChange(6); // => 8
+console.log(c)
+
+
+

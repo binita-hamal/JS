4214890a1aacb308dd23aced5df61caedb919122
winiiee <binita.hamal7@gmail.com>
2025-07-09 22:16:00 +0545

    added-how-equal

--- workshops/03-coercion-and-truthiness/01-how-equal/how-equal.js
+++ workshops/03-coercion-and-truthiness/01-how-equal/how-equal.js
@@ -1,1 +1,16 @@
 // YOUR CODE BELOW
+
+function howEqual(a,b){
+    if(a===b){
+        return 'strictly'
+    }
+    else if(a==b){
+        return "loosely"
+    }
+    else{
+        return "not equal"
+    }
+}
+
+let a =howEqual(0, '0'); // => loosely
+console.log(a)

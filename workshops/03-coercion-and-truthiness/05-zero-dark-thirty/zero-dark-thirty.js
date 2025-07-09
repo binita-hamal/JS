4b42c040534f87071bba10880966c8e2b6fb3b60
winiiee <binita.hamal7@gmail.com>
2025-07-09 22:17:13 +0545

    added-zero-dark-thirty

--- workshops/03-coercion-and-truthiness/05-zero-dark-thirty/zero-dark-thirty.js
+++ workshops/03-coercion-and-truthiness/05-zero-dark-thirty/zero-dark-thirty.js
@@ -1,1 +1,18 @@
-// YOUR CODE BELOW
+function zeroDarkThirty(num){
+    if(num ===0){
+        return NaN;
+    }
+    let string = String(num)
+    let s=''
+    for(let i=0; i<string.length; i++){
+        if(string[i] ==='0'){
+            continue;
+        }
+        s= s + string[i] //string[i] + s
+    }
+    return Number(s)
+
+}
+
+let a = zeroDarkThirty(10) // => 1232
+console.log(a)

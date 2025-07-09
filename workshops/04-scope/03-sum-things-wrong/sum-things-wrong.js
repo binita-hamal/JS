549a8b1b18b497a09aa57d375a25d6fddf8b6e01
winiiee <binita.hamal7@gmail.com>
2025-07-09 22:14:43 +0545

    added-sum-things-wrong

--- workshops/04-scope/03-sum-things-wrong/sum-things-wrong.js
+++ workshops/04-scope/03-sum-things-wrong/sum-things-wrong.js
@@ -1,8 +1,14 @@
-let sum = 0;
 
 function sumThingsWrong(num1, num2) {
+  let sum = 0;
   sum += num1;
   sum += num2;
-
   return sum;
 }
+
+let a =sumThingsWrong(50, 100);
+console.log(a);
+let b = sumThingsWrong(5, -10);
+console.log(b);
+let c  = sumThingsWrong(-1, -1);
+console.log(c);

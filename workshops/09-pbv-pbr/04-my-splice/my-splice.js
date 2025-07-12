b9c0e2c190e635cf12b4d9d3876a7a3fbd39056d
winiiee <binita.hamal7@gmail.com>
2025-07-12 21:52:49 +0545

    my-splice-added

--- workshops/09-pbv-pbr/04-my-splice/my-splice.js
+++ workshops/09-pbv-pbr/04-my-splice/my-splice.js
@@ -1,1 +1,40 @@
-// YOUR CODE BELOW
+function mySplice(array, startIdx, deleteCount, item) {
+  debugger;
+  let removeItem = [];
+  let keepItem = [];
+
+  //let keep the items before the start index
+
+  for (let i = 0; i < startIdx; i++) {
+    keepItem.push(array[i]); //keep =[1];
+  }
+
+  if (item !== undefined) {
+    keepItem.push(item); //keep=[1,'item'];
+  }
+
+  //keep removed element
+  for (let j = startIdx; j < startIdx + deleteCount; j++) {
+    removeItem.push(array[j]); //remove =[2];
+  }
+
+  //push remaining elements
+  for (let i = startIdx + deleteCount; i < array.length; i++) {
+    keepItem.push(array[i]);
+  }
+let len = array.length;
+  for (let k = 0; k < len; k++) {
+    array.pop();
+  }
+
+  for (let m = 0; m < keepItem.length; m++) {
+    array.push(keepItem[m]);
+  }
+  return removeItem;
+}
+
+
+
+let ourStuff = ['food', 'trash', 'clothes'];
+
+mySplice(ourStuff, 1, 1);

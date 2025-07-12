51e87cec0cb6a6e26ac73996400103a3bdc9b6d9
winiiee <binita.hamal7@gmail.com>
2025-07-12 21:52:17 +0545

    very-odd-mutant-added

--- workshops/09-pbv-pbr/02-very-odd-mutant/very-odd-mutant.js
+++ workshops/09-pbv-pbr/02-very-odd-mutant/very-odd-mutant.js
@@ -1,1 +1,20 @@
 // YOUR CODE BELOW
+
+function veryOddMutant(array){
+    let count =0;
+    for(let i=0; i<array.length; i++){
+        if(array[i] %2 ===0 ){
+            array[i] = "normie";
+            count = count + 1;
+        }
+    }
+    return count;
+}
+
+// let allTheNums = [1, 2, 3, 4, 5, 6, 7, 8,9,10];
+// let countRemoved = veryOddMutant(allTheNums);
+
+// console.log(allTheNums);
+// // [1, 'normie', 3, 'normie', 5, 'normie', 7, 'normie'];
+// console.log('countRemoved:', count);
+// // 4

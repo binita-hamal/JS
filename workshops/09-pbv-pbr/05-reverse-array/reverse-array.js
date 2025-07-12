61e57d876dc8b26c88b257abe4fbb624cde23bcf
winiiee <binita.hamal7@gmail.com>
2025-07-12 21:53:05 +0545

    reverse-array-added

--- workshops/09-pbv-pbr/05-reverse-array/reverse-array.js
+++ workshops/09-pbv-pbr/05-reverse-array/reverse-array.js
@@ -1,1 +1,15 @@
-// YOUR CODE BELOW
+
+function reverseArray(array) {
+    debugger;
+  for (let i = 0; i < array.length/2; i++) {
+    let temp = array[i];
+    let j = array.length - 1 - i;
+    array[i] = array[j];
+    array[j] = temp;
+  }
+    return array;
+}
+
+reverseArray([1,2,3,4]);
+
+

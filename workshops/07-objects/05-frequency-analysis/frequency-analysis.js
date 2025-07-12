642dff2939fd88fb60124a460902f64f2d00aca5
winiiee <binita.hamal7@gmail.com>
2025-07-12 21:50:28 +0545

    frequency-analysis-added

--- workshops/07-objects/05-frequency-analysis/frequency-analysis.js
+++ workshops/07-objects/05-frequency-analysis/frequency-analysis.js
@@ -1,1 +1,22 @@
 // YOUR CODE BELOW
+// frequencyAnalysis('abca'); // => {a: 2, b: 1, c: 1}
+
+function frequencyAnalysis(string){
+    let newObj ={}
+    
+    for(let char of string){
+        if(newObj[char]){
+            newObj[char] = newObj[char] + 1;
+
+        }
+        else{
+            newObj[char] = 1;
+        }
+    }
+
+    return newObj;
+
+}
+
+let o = frequencyAnalysis('abca'); // => {a: 2, b: 1, c: 1}
+console.log(o);

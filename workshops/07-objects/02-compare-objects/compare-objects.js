c9b52508a9246750f2e3d39c797911a7f3989224
winiiee <binita.hamal7@gmail.com>
2025-07-12 21:49:40 +0545

    compare-objects-added

--- workshops/07-objects/02-compare-objects/compare-objects.js
+++ workshops/07-objects/02-compare-objects/compare-objects.js
@@ -1,1 +1,22 @@
 // YOUR CODE BELOW
+
+function compareObjects(obj1,obj2){
+    let k1=Object.keys(obj1)
+    let k2=Object.keys(obj2);
+
+    if(k1.length !== k2.length){
+        return false;
+    }
+
+    for(let key of k1){
+        if(obj1[key]!==obj2[key]){
+            return false;
+        }
+    }
+
+    return true;
+    
+}
+
+let c = compareObjects({ name: 'nick' }, { name: 'nick' }) // -> true
+console.log(c);

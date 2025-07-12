bef50a9aae6924844f28c344492051b13d300893
winiiee <binita.hamal7@gmail.com>
2025-07-12 21:50:50 +0545

    dog-breeder-added

--- workshops/07-objects/06-dog-breeder/dog-breeder.js
+++ workshops/07-objects/06-dog-breeder/dog-breeder.js
@@ -1,1 +1,25 @@
 // YOUR CODE BELOW
+
+function dogBreeder(a, b) {
+  let obj = {};
+  let name = "Steve";
+  let age = 10;
+
+  if (typeof a === "string") {
+    name = a;
+    if (typeof b === "number") {
+      age = b;
+    }
+  } else if (typeof a === "number") {
+    age = a;
+  }
+  return {
+    name: name,
+    age: age,
+  };
+}
+let d = dogBreeder("Sam", 12); // => {name: 'Sam', age: 12}
+console.log(d);
+
+let e = dogBreeder("bini"); // => {name:'Steve', age: 15}
+console.log(e);

62b560b608ed58600bd6eccea58e9331ede5e9b4
winiiee <binita.hamal7@gmail.com>
2025-07-12 21:52:34 +0545

    clone-machine-added

--- workshops/09-pbv-pbr/03-clone-machine/clone-machine.js
+++ workshops/09-pbv-pbr/03-clone-machine/clone-machine.js
@@ -1,1 +1,25 @@
-// YOUR CODE BELOW
+
+function cloneMachine(object){
+  console.log(object['name']);
+
+    let newObj = {};
+    newObj['name'] = object['name'] + 'Clone';
+
+    newObj['species'] = object['species'];
+    newObj['offspring'] =[];
+    object['offspring'].push(newObj['name']);
+    return newObj;
+    
+
+}
+
+let dolly = {
+    name: 'Dolly',
+    species: 'sheep',
+    offspring: []
+  }
+let a =cloneMachine(dolly);
+let b =cloneMachine(a);
+console.log(a);
+console.log(b);
+console.log(dolly) // {name: 'Dolly', species: 'sheep', offspring: ['DollyClone']}

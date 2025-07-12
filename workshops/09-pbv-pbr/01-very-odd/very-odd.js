712388b9771edeac4462bd371df23086fa46a8d9
winiiee <binita.hamal7@gmail.com>
2025-07-12 21:51:51 +0545

    very-odd-added

--- workshops/09-pbv-pbr/01-very-odd/very-odd.js
+++ workshops/09-pbv-pbr/01-very-odd/very-odd.js
@@ -1,1 +1,18 @@
 // YOUR CODE BELOW
+
+
+function veryOdd(array){
+    let onlyOddArray=[];
+    for(let i=0; i<array.length; i++){
+        if(array[i] % 2 !== 0){
+            onlyOddArray.push(array[i]);
+        }
+    }
+    return onlyOddArray;
+}
+
+let allTheNums = [1, 2, 3, 4, 5, 6, 7, 8];
+let oddNums = veryOdd(allTheNums);
+
+console.log('oddNums:', oddNums); // [1, 3, 5, 7];
+console.log('allTheNums:', allTheNums); //

fdb0133e579b659a6cd8366a5ec4db651f6eefc8
winiiee <binita.hamal7@gmail.com>
2025-07-12 21:53:31 +0545

    deeper-copy-added

--- workshops/09-pbv-pbr/06-deeper-copy/deeper-copy.js
+++ workshops/09-pbv-pbr/06-deeper-copy/deeper-copy.js
@@ -1,1 +1,24 @@
 // YOUR CODE BELOW
+let myArray = [1, [2, 3]];
+function deeperCopy(array) {
+  let parent = [];
+  for (let i = 0; i < array.length; i++) {
+    // 1
+
+    if (Array.isArray(array[i])) {
+      let child = [];
+      for (let j = 0; j < array[i].length; j++) {
+        child.push(array[i][j]);
+      }
+      parent.push(child);
+    } 
+
+
+    else {
+      parent.push(array[i]);
+    }
+
+    
+  }
+  return parent;
+}

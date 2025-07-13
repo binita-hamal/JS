b4907cccb7fb2de44d443187721e3d71e7c5215d
winiiee <binita.hamal7@gmail.com>
2025-07-13 09:34:05 +0545

    function-logger-added

--- workshops/10-higher-order-functions-i/01-function-logger/function-logger.js
+++ workshops/10-higher-order-functions-i/01-function-logger/function-logger.js
@@ -1,1 +1,7 @@
-// YOUR CODE BELOW
+//
+function functionLogger(someFunc,someArg){
+    console.log("function starting");
+    let r=someFunc(someArg);
+    console.log("Function complete");
+    return r;
+}

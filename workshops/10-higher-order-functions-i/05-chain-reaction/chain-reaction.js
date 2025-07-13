503233f2c137a462162d7e6d4148c5412570ef0d
winiiee <binita.hamal7@gmail.com>
2025-07-13 09:35:14 +0545

    chain-reaction-added

--- workshops/10-higher-order-functions-i/05-chain-reaction/chain-reaction.js
+++ workshops/10-higher-order-functions-i/05-chain-reaction/chain-reaction.js
@@ -1,1 +1,24 @@
-// YOUR CODE BELOW
+function addTen(num) {
+  return num + 10;
+}
+
+function subtractFive(num) {
+  return num - 5;
+}
+
+function multiplyFive(num) {
+  return num * 5;
+}
+
+function chainReaction(start,arrayOfFuncs){
+    let result = start; //result =0;
+    arrayOfFuncs.forEach((func) =>{
+        result = func(result);
+    })
+
+    return result;
+
+}
+
+let a = [addTen, subtractFive, multiplyFive]
+chainReaction(0,a);

24aead4ed0c69275486b06b9102a51495587163b
winiiee <binita.hamal7@gmail.com>
2025-07-13 09:34:26 +0545

    finder-function-added

--- workshops/10-higher-order-functions-i/02-finder-function/finder-function.js
+++ workshops/10-higher-order-functions-i/02-finder-function/finder-function.js
@@ -1,1 +1,17 @@
 // YOUR CODE BELOW
+function func(){
+    return true;
+}
+
+function finderFunction(array,func){
+    for(let i=0; i<array.length;i++){
+    if(true){
+        return array[i];
+        
+    }
+    else{
+        return -1;
+    }
+
+}
+}

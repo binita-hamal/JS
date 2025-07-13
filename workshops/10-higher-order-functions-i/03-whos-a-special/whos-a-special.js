9c4e9427808c3d50865d3632513d772a625859cb
winiiee <binita.hamal7@gmail.com>
2025-07-13 09:34:45 +0545

    whos-a-special-added

--- workshops/10-higher-order-functions-i/03-whos-a-special/whos-a-special.js
+++ workshops/10-higher-order-functions-i/03-whos-a-special/whos-a-special.js
@@ -1,1 +1,20 @@
-// YOUR CODE BELOW
+
+function whosASpecial(pets){
+    let result ='';
+    pets.forEach((pet) =>{
+        result = result + pet.name + ' the ' + pet.species + ' is very special! ';
+    })
+    return result.trim();
+    
+}
+
+let specialPets = [{
+    name: 'Mr. Binns',
+    species: 'cat'
+  }, {
+    name: 'Pusheen',
+    species: 'cat'
+  }
+];
+
+let r = whosASpecial(specialPets);
